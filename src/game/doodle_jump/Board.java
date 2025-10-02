74a750dc115e33ca670bba9b0c784c99228073e5
CarFix77 <ruslanshibayev2007@mail.ru>
2025-10-02 10:35:23 +0300

    Update Board.java

--- src/game/doodle_jump/Board.java
+++ src/game/doodle_jump/Board.java
@@ -1,186 +1,185 @@
-package game.doodle_jump;
-
-import game.engine.Texture;
-
-import java.util.*;
-import android.graphics.*;
-
-public class Board {
-	protected double x,y;
-	Player doodle;
-	Doodlejump doodlejump;
-	public static final int board_length=120;
-	public Board(double _x,double _y,Doodlejump doodlejump,Player player)
-	{
-		this.doodlejump=doodlejump;
-		x=_x;y=_y;
-		doodle=player;
-	}
-	void move_y(double dt){y+=dt*doodle.vy;}
-	double get_x() {return x;}
-	double get_y() {return y;}
-	boolean is_click() { // Judge if player clicks the board
-		return (doodle.y-get_y()>=0 && doodle.y-get_y()<=25 && 
-				doodle.x>=x-Player.length+10 && doodle.x<=x+board_length-10);
-	}
-	int get_state() {return 0;} // Judge if player gets the item
-	void go_ahead(double t) {} // Blue board and brown board: move
-	boolean destroy() {return false;} // White board: destroy
-	boolean disappear(double dt) {return false;} // Yellow board: disappear
-	int get_id() {return 0;} // Return board id
-	boolean has_item() {return false;}
-	void down() {} // If there is item on the board: item down
-	void destroy_item(int id) {}
-	double get_left() {return -1;} // Return the left distance of item on board
-	Item get_item() {return null;}
-	void draw_item(Canvas canvas,Paint paint,Texture texture) {}
+// Board.js - базовый класс для платформ
+class Board {
+    constructor(_x, _y, doodlejump, player) {
+        this.x = _x;
+        this.y = _y;
+        this.doodle = player;
+        this.doodlejump = doodlejump;
+    }
+    
+    static get BOARD_LENGTH() { return 120; }
+    
+    move_y(dt) {
+        this.y += dt * this.doodle.vy;
+    }
+    
+    get_x() { return this.x; }
+    get_y() { return this.y; }
+    
+    is_click() {
+        // Проверка столкновения игрока с платформой
+        return (this.doodle.y - this.get_y() >= 0 && 
+                this.doodle.y - this.get_y() <= 25 && 
+                this.doodle.x >= this.x - Player.LENGTH + 10 && 
+                this.doodle.x <= this.x + Board.BOARD_LENGTH - 10);
+    }
+    
+    get_state() { return 0; } // Проверка получения предмета
+    go_ahead(t) {} // Движение для синих и коричневых платформ
+    destroy() { return false; } // Уничтожение белой платформы
+    disappear(dt) { return false; } // Исчезновение желтой платформы
+    get_id() { return 0; } // ID типа платформы
+    has_item() { return false; }
+    down() {} // Падение предмета с платформы
+    destroy_item(id) {}
+    get_left() { return -1; }
+    get_item() { return null; }
+    draw_item(ctx, texture) {} // Отрисовка предмета на canvas
 }
 
+// Зеленая платформа (может содержать предметы)
 class Greenboard extends Board {
-	private double left;
-	Item item;
-	public Greenboard(double _x,double _y,int _id,double __x,
-			Doodlejump doodlejump,Player player) {
-		super(_x, _y,doodlejump,player);
-		if (_id!=0)
-		{
-			item=new Item(_id);
-			left=__x;
-			item.set_rect((int)x, (int)y, (int)left);
-		}
-		else
-		{
-			item=null;
-			left=-1;
-		}
-	}
-	boolean has_item() {return item!=null;}
-	Item get_item() {return item;}
-	void destroy_item(int id) {if (id!=1) item=null;}
-	int get_state() {
-		if (item==null) return 0;
-		switch (item.id) {
-			case spring:
-				if (doodle.x>=x+get_left()-Player.length && 
-					doodle.x<=x+get_left()+Item.spring_length)
-						return 1;
-			break;
-			case hat:
-				if (doodle.x>=x+get_left()-Player.length &&
-					doodle.x<=x+get_left()+Item.hat_length)
-				{
-					destroy_item(2);
-					return 2;
-				}
-			break;
-			case rocket:
-				if (doodle.x>=x+get_left()-Player.length &&
-					doodle.x<=x+get_left()+Item.rocket_length)
-				{
-					destroy_item(3);
-					return 3;
-				}
-			break;
-			case shoes:
-				if (doodle.x>=x+get_left()-Player.length &&
-					doodle.x<=x+get_left()+Item.shoes_length)
-				{
-					destroy_item(4);
-					return 4;
-				}
-			break;
-		}
-		return 0;
-	}
-	void down() {
-		if (item!=null)
-			item.set_rect((int)x, (int)y, (int)left);
-	}
-	double get_left() {return left;}
-	int get_id() {return 0;}
-	void draw_item(Canvas canvas,Paint paint,Texture texture) {
-		if (item!=null)
-			item.draw(canvas, paint, texture);
-	}
+    constructor(_x, _y, _id, __x, doodlejump, player) {
+        super(_x, _y, doodlejump, player);
+        this.left = -1;
+        this.item = null;
+        
+        if (_id !== 0) {
+            this.item = new Item(_id);
+            this.left = __x;
+            this.item.set_rect(Math.floor(this.x), Math.floor(this.y), Math.floor(this.left));
+        }
+    }
+    
+    has_item() { return this.item !== null; }
+    get_item() { return this.item; }
+    destroy_item(id) { if (id !== 1) this.item = null; }
+    
+    get_state() {
+        if (this.item === null) return 0;
+        
+        switch (this.item.id) {
+            case Item.SPRING:
+                if (this.doodle.x >= this.x + this.get_left() - Player.LENGTH && 
+                    this.doodle.x <= this.x + this.get_left() + Item.SPRING_LENGTH)
+                    return 1;
+                break;
+            case Item.HAT:
+                if (this.doodle.x >= this.x + this.get_left() - Player.LENGTH &&
+                    this.doodle.x <= this.x + this.get_left() + Item.HAT_LENGTH) {
+                    this.destroy_item(2);
+                    return 2;
+                }
+                break;
+            case Item.ROCKET:
+                if (this.doodle.x >= this.x + this.get_left() - Player.LENGTH &&
+                    this.doodle.x <= this.x + this.get_left() + Item.ROCKET_LENGTH) {
+                    this.destroy_item(3);
+                    return 3;
+                }
+                break;
+            case Item.SHOES:
+                if (this.doodle.x >= this.x + this.get_left() - Player.LENGTH &&
+                    this.doodle.x <= this.x + this.get_left() + Item.SHOES_LENGTH) {
+                    this.destroy_item(4);
+                    return 4;
+                }
+                break;
+        }
+        return 0;
+    }
+    
+    down() {
+        if (this.item !== null)
+            this.item.set_rect(Math.floor(this.x), Math.floor(this.y), Math.floor(this.left));
+    }
+    
+    get_left() { return this.left; }
+    get_id() { return 0; }
+    
+    draw_item(ctx, texture) {
+        if (this.item !== null)
+            this.item.draw(ctx, texture);
+    }
 }
 
+// Синяя движущаяся платформа
 class Blueboard extends Board {
-	private double speed;
-	public Blueboard(double _x,double _y,Doodlejump doodlejump,Player player) {
-		super(_x, _y,doodlejump,player);
-		speed=0;
-		while (speed<50)
-			speed=(double)new Random().nextInt(150);
-	}
-	void go_ahead(double dt) {
-		if (x>0 && x<doodlejump.width-board_length)
-		{
-			x+=speed*dt;
-			return;
-		}
-		speed=-speed;
-		while (x>doodlejump.width-board_length || x<0)
-			x+=speed*dt;
-	}
-	int get_id() {return 1;}
+    constructor(_x, _y, doodlejump, player) {
+        super(_x, _y, doodlejump, player);
+        this.speed = 0;
+        while (this.speed < 50)
+            this.speed = Math.floor(Math.random() * 150);
+    }
+    
+    go_ahead(dt) {
+        if (this.x > 0 && this.x < this.doodlejump.width - Board.BOARD_LENGTH) {
+            this.x += this.speed * dt;
+            return;
+        }
+        this.speed = -this.speed;
+        while (this.x > this.doodlejump.width - Board.BOARD_LENGTH || this.x < 0)
+            this.x += this.speed * dt;
+    }
+    
+    get_id() { return 1; }
 }
 
+// Коричневая платформа (движется вверх-вниз)
 class Brownboard extends Board {
-	private double speed,up,has_up;
-	public Brownboard(double _x,double _y,Doodlejump doodlejump,Player player) {
-		super(_x,_y,doodlejump,player);
-		speed=up=has_up=0;
-		while (up<50)
-			up=(double)new Random().nextInt(200);
-		while (speed<50)
-			speed=(double)new Random().nextInt(150);
-	}
-	int get_id() {return 2;}
-	double get_y() {return y+has_up;}
-	void go_ahead(double dt) {
-		if (has_up<=up && has_up>=-up)
-		{
-			has_up+=speed*dt;
-			return;
-		}
-		speed=-speed;
-		while (has_up>up || has_up<=-up)
-			has_up+=speed*dt;
-	}
+    constructor(_x, _y, doodlejump, player) {
+        super(_x, _y, doodlejump, player);
+        this.speed = this.up = this.has_up = 0;
+        while (this.up < 50)
+            this.up = Math.floor(Math.random() * 200);
+        while (this.speed < 50)
+            this.speed = Math.floor(Math.random() * 150);
+    }
+    
+    get_id() { return 2; }
+    get_y() { return this.y + this.has_up; }
+    
+    go_ahead(dt) {
+        if (this.has_up <= this.up && this.has_up >= -this.up) {
+            this.has_up += this.speed * dt;
+            return;
+        }
+        this.speed = -this.speed;
+        while (this.has_up > this.up || this.has_up <= -this.up)
+            this.has_up += this.speed * dt;
+    }
 }
 
+// Белая платформа (уничтожается при приземлении)
 class Whiteboard extends Board {
-	public Whiteboard(double _x,double _y,Doodlejump doodlejump,Player player) 
-	{super(_x, _y,doodlejump,player);}
-	boolean destroy() {return true;}
-	int get_id() {return 3;}
+    constructor(_x, _y, doodlejump, player) {
+        super(_x, _y, doodlejump, player);
+    }
+    
+    destroy() { return true; }
+    get_id() { return 3; }
 }
 
+// Желтая платформа (исчезает со временем)
 class Yellowboard extends Board {
-	private double decrease;
-	public Yellowboard(double _x,double _y,Doodlejump doodlejump,Player player) {
-		super(_x, _y,doodlejump,player);
-		decrease=6;
-	}
-	boolean disappear(double dt) {
-		if (get_y()<0)
-			return false;
-		decrease-=dt;
-		if (decrease>=0)
-			return false;
-		return true;
-	}
-	int get_id() {
-		if (decrease>=3)
-			return 4;
-		if (decrease>=1.5)
-			return 5;
-		if (decrease>=0.7)
-			return 6;
-		if (decrease>=0.35)
-			return 7;
-		if (decrease>=0.175)
-			return 8;
-		return 9;
-	}
+    constructor(_x, _y, doodlejump, player) {
+        super(_x, _y, doodlejump, player);
+        this.decrease = 6;
+    }
+    
+    disappear(dt) {
+        if (this.get_y() < 0) return false;
+        this.decrease -= dt;
+        if (this.decrease >= 0) return false;
+        return true;
+    }
+    
+    get_id() {
+        if (this.decrease >= 3) return 4;
+        if (this.decrease >= 1.5) return 5;
+        if (this.decrease >= 0.7) return 6;
+        if (this.decrease >= 0.35) return 7;
+        if (this.decrease >= 0.175) return 8;
+        return 9;
+    }
 }
