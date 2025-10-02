628fac097d89b73c2c3062dc98f58512bf6ca4ca
CarFix77 <ruslanshibayev2007@mail.ru>
2025-10-02 13:06:26 +0300

    Update Bullet.java

--- src/game/doodle_jump/Bullet.java
+++ src/game/doodle_jump/Bullet.java
@@ -1,42 +1,61 @@
-package game.doodle_jump;
-
-import android.graphics.*;
-
-public class Bullet {
-	Doodlejump doodlejump;
-	Player doodle;
-	Bitmap bitmap;
-	Rect rect,tRect;
-	
-	private double x,y,vx,vy;
-	public Bullet(double _x,double _y,Player player,Doodlejump _doodle) {
-		//bullet=new Texture(_doodle);
-		x=_x;y=_y;
-		doodle=player;
-		doodlejump=_doodle;
-		bitmap=doodlejump.ball.getBitmap(96, 64, 32, 32);
-		if (!doodlejump.dir_shoot || doodlejump.monster==null) {vx=0;vy=700;}
-		else if (doodlejump.monster.y+Monster.monster_height/2>=y)
-		{
-			vx=0;vy=700;
-		}
-		else
-		{
-			double delta_x=doodlejump.monster.x+Monster.monster_length/2-x,
-					delta_y=y-doodlejump.monster.y+Monster.monster_height/2;
-			double delta=Math.sqrt(delta_x*delta_x+delta_y*delta_y);
-			vx=700*delta_x/delta;
-			vy=700*delta_y/delta;
-		}
-	}
-	void go(double dt) { x+=vx*dt;vy-=Doodlejump.g*dt;y-=vy*dt;}
-	void down(double dt) {y+=dt*doodle.vy;}
-	boolean shoot() {
-		return (x>doodlejump.monster.x && x<doodlejump.monster.x+Monster.monster_length 
-				&& y>doodlejump.monster.y-Monster.monster_height && y<doodlejump.monster.y);
-	}
-	boolean destroy() {return (x<0 || x>doodlejump.width || y>doodlejump.height);}
-	void draw(Canvas canvas,Paint paint) {
-		canvas.drawBitmap(bitmap, (float)x, (float)y, paint);
-	}
+class Bullet {
+    constructor(_x, _y, player, _doodle) {
+        this.x = _x;
+        this.y = _y;
+        this.doodle = player;
+        this.doodlejump = _doodle;
+        
+        // Загрузка изображения пули
+        this.image = this.doodlejump.textures.ball;
+        
+        if (!this.doodlejump.dir_shoot || this.doodlejump.monster == null) {
+            this.vx = 0;
+            this.vy = 700;
+        } else if (this.doodlejump.monster.y + Monster.MONSTER_HEIGHT / 2 >= this.y) {
+            this.vx = 0;
+            this.vy = 700;
+        } else {
+            const delta_x = this.doodlejump.monster.x + Monster.MONSTER_LENGTH / 2 - this.x;
+            const delta_y = this.y - this.doodlejump.monster.y + Monster.MONSTER_HEIGHT / 2;
+            const delta = Math.sqrt(delta_x * delta_x + delta_y * delta_y);
+            this.vx = 700 * delta_x / delta;
+            this.vy = 700 * delta_y / delta;
+        }
+    }
+    
+    go(dt) {
+        this.x += this.vx * dt;
+        this.vy -= Doodlejump.G * dt;
+        this.y -= this.vy * dt;
+    }
+    
+    down(dt) {
+        this.y += dt * this.doodle.vy;
+    }
+    
+    shoot(monster) {
+        if (!monster) return false;
+        return (this.x > monster.x && 
+                this.x < monster.x + Monster.MONSTER_LENGTH && 
+                this.y > monster.y - Monster.MONSTER_HEIGHT && 
+                this.y < monster.y);
+    }
+    
+    destroy() {
+        return (this.x < 0 || 
+                this.x > this.doodlejump.width || 
+                this.y > this.doodlejump.height);
+    }
+    
+    draw(ctx) {
+        if (this.image) {
+            ctx.drawImage(this.image, this.x, this.y, 32, 32);
+        } else {
+            // Fallback: рисуем круг если нет текстуры
+            ctx.fillStyle = 'black';
+            ctx.beginPath();
+            ctx.arc(this.x + 16, this.y + 16, 8, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    }
 }
